b07601ca28f72e294dec1b5c6b9dde604282b914
Hongda Hu <hdhu@thoughtworks.com>
2014-07-12 15:59:34 +0800

    [jerry] add recipe for mongod & redis-server

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'chef-monit-configuration'
+maintainer 'jerry'
+maintainer_email 'jerrery520@gmail.com'
+description 'this is monit configuration for monit, about server components'
+version '0.0.1'
+
+
+recipe "chef-monit-configuration::redis-server", "Monit config for redis server"
+recipe "chef-monit-configuration::mongod", "Monit config for redis server"
